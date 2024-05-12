287b4749f8444a66580c899515a3ac3b258711b2
benjamin-Olguin <benjamin.olguin@usm.cl>
2024-05-12 03:07:55 -0400

    Nueva BD

--- la_diversion.sql
+++ la_diversion.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 12-05-2024 a las 02:40:51
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2024 at 09:07 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `la_diversion`
+-- Database: `la_diversion`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `clientes`
+-- Table structure for table `clientes`
 --
 
 CREATE TABLE `clientes` (
@@ -35,7 +35,7 @@ CREATE TABLE `clientes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `clientes`
+-- Dumping data for table `clientes`
 --
 
 INSERT INTO `clientes` (`Id_Cliente`, `Rut`, `Numero_habitacion`, `Valor_Acumulado`) VALUES
@@ -44,7 +44,7 @@ INSERT INTO `clientes` (`Id_Cliente`, `Rut`, `Numero_habitacion`, `Valor_Acumula
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `habitaciones`
+-- Table structure for table `habitaciones`
 --
 
 CREATE TABLE `habitaciones` (
@@ -55,20 +55,20 @@ CREATE TABLE `habitaciones` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `habitaciones`
+-- Dumping data for table `habitaciones`
 --
 
 INSERT INTO `habitaciones` (`Numero_habitacion`, `Tipo_habitacion`, `Precio`, `Estado`) VALUES
 (101, 'Single', 50, 1),
 (102, 'Double', 75, 1),
-(103, 'King', 100, 0),
-(104, 'Single', 50, 0),
+(103, 'King', 100, 1),
+(104, 'Single', 50, 1),
 (105, 'King', 100, 0);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `precios_diarios`
+-- Table structure for table `precios_diarios`
 --
 
 CREATE TABLE `precios_diarios` (
@@ -82,7 +82,7 @@ CREATE TABLE `precios_diarios` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `reservas`
+-- Table structure for table `reservas`
 --
 
 CREATE TABLE `reservas` (
@@ -94,17 +94,31 @@ CREATE TABLE `reservas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `reservas`
+-- Dumping data for table `reservas`
 --
 
 INSERT INTO `reservas` (`Id_Reserva`, `Rut`, `Numero_habitacion`, `Fecha_Checkin`, `Fecha_CheckOut`) VALUES
 (44, 12983432, 101, '2024-05-17', '2024-05-24'),
-(45, 12983432, 102, '2024-05-17', '2024-05-24');
+(45, 12983432, 102, '2024-05-17', '2024-05-24'),
+(46, 12345678, 103, '0001-01-01', '0001-01-01'),
+(47, 12345679, 104, '0001-01-01', '0001-01-05');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tours`
+-- Table structure for table `reservas_tours`
+--
+
+CREATE TABLE `reservas_tours` (
+  `Id_Reserva_Tour` int(11) NOT NULL,
+  `Id_Tour` int(11) NOT NULL,
+  `Id_Reserva` int(11) NOT NULL COMMENT 'id de reserva de habitacion'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tours`
 --
 
 CREATE TABLE `tours` (
@@ -112,110 +126,123 @@ CREATE TABLE `tours` (
   `Fecha` date NOT NULL,
   `Lugar` varchar(255) NOT NULL,
   `Medio_Transporte` varchar(100) NOT NULL,
-  `Imagen` varchar(255) NOT NULL
+  `Imagen` varchar(255) NOT NULL,
+  `Precio` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `tours`
+-- Dumping data for table `tours`
 --
 
-INSERT INTO `tours` (`Id_Tour`, `Fecha`, `Lugar`, `Medio_Transporte`, `Imagen`) VALUES
-(1, '2024-05-20', 'Parque Nacional', 'Autobús', 'imagen_parque_nacional.jpg'),
-(2, '2024-06-15', 'Museo de Historia', 'Caminando', 'imagen_museo_historia.jpg'),
-(3, '2024-07-01', 'Playa Los Cocos', 'Barco', 'imagen_playa_los_cocos.jpg'),
-(4, '2024-08-20', 'Montañas El Gran Pico', 'Teleférico', 'imagen_montanas_gran_pico.jpg'),
-(5, '2024-09-05', 'Valle de los Reyes', 'Caballo', 'imagen_valle_reyes.jpg');
+INSERT INTO `tours` (`Id_Tour`, `Fecha`, `Lugar`, `Medio_Transporte`, `Imagen`, `Precio`) VALUES
+(1, '0001-01-01', 'Parque Nacional', 'Autobús', 'imagen_parque_nacional.jpg', 10),
+(2, '0001-01-02', 'Museo de Historia', 'Caminando', 'imagen_museo_historia.jpg', 20),
+(3, '0001-01-03', 'Playa Los Cocos', 'Barco', 'imagen_playa_los_cocos.jpg', 30),
+(4, '0001-01-04', 'Montañas El Gran Pico', 'Teleférico', 'imagen_montanas_gran_pico.jpg', 40),
+(5, '0001-01-05', 'Valle de los Reyes', 'Caballo', 'imagen_valle_reyes.jpg', 50);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `clientes`
+-- Indexes for table `clientes`
 --
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`Id_Cliente`),
   ADD KEY `Numero_habitacion` (`Numero_habitacion`);
 
 --
--- Indices de la tabla `habitaciones`
+-- Indexes for table `habitaciones`
 --
 ALTER TABLE `habitaciones`
   ADD PRIMARY KEY (`Numero_habitacion`);
 
 --
--- Indices de la tabla `precios_diarios`
+-- Indexes for table `precios_diarios`
 --
 ALTER TABLE `precios_diarios`
   ADD PRIMARY KEY (`Id`),
   ADD KEY `fk_numero_habitacion` (`Numero_habitacion`);
 
 --
--- Indices de la tabla `reservas`
+-- Indexes for table `reservas`
 --
 ALTER TABLE `reservas`
   ADD PRIMARY KEY (`Id_Reserva`),
   ADD KEY `Numero Habitacion` (`Numero_habitacion`);
 
 --
--- Indices de la tabla `tours`
+-- Indexes for table `reservas_tours`
+--
+ALTER TABLE `reservas_tours`
+  ADD PRIMARY KEY (`Id_Reserva_Tour`);
+
+--
+-- Indexes for table `tours`
 --
 ALTER TABLE `tours`
   ADD PRIMARY KEY (`Id_Tour`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `clientes`
+-- AUTO_INCREMENT for table `clientes`
 --
 ALTER TABLE `clientes`
   MODIFY `Id_Cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de la tabla `habitaciones`
+-- AUTO_INCREMENT for table `habitaciones`
 --
 ALTER TABLE `habitaciones`
   MODIFY `Numero_habitacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
 
 --
--- AUTO_INCREMENT de la tabla `precios_diarios`
+-- AUTO_INCREMENT for table `precios_diarios`
 --
 ALTER TABLE `precios_diarios`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `reservas`
+-- AUTO_INCREMENT for table `reservas`
 --
 ALTER TABLE `reservas`
-  MODIFY `Id_Reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `Id_Reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
--- AUTO_INCREMENT de la tabla `tours`
+-- AUTO_INCREMENT for table `reservas_tours`
+--
+ALTER TABLE `reservas_tours`
+  MODIFY `Id_Reserva_Tour` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tours`
 --
 ALTER TABLE `tours`
   MODIFY `Id_Tour` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `clientes`
+-- Constraints for table `clientes`
 --
 ALTER TABLE `clientes`
   ADD CONSTRAINT `clientes_ibfk_1` FOREIGN KEY (`Numero_habitacion`) REFERENCES `habitaciones` (`Numero_habitacion`);
 
 --
--- Filtros para la tabla `precios_diarios`
+-- Constraints for table `precios_diarios`
 --
 ALTER TABLE `precios_diarios`
   ADD CONSTRAINT `fk_numero_habitacion` FOREIGN KEY (`Numero_habitacion`) REFERENCES `habitaciones` (`Numero_habitacion`),
   ADD CONSTRAINT `fk_precios_diarios_habitaciones` FOREIGN KEY (`Numero_habitacion`) REFERENCES `habitaciones` (`Numero_habitacion`);
 
 --
--- Filtros para la tabla `reservas`
+-- Constraints for table `reservas`
 --
 ALTER TABLE `reservas`
   ADD CONSTRAINT `Numero Habitacion` FOREIGN KEY (`Numero_habitacion`) REFERENCES `habitaciones` (`Numero_habitacion`) ON DELETE NO ACTION ON UPDATE NO ACTION;
