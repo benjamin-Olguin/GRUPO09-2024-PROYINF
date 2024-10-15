0d6e8fce0b6f9b27fb5a408541c3f479ec5690ff
benjamin.Olguin <benjamin.olguin@usm.cl>
2024-10-15 20:18:21 -0300

    postgres

---
+++ dbConfig.js
@@ -0,0 +1,20 @@
+import pg from 'pg';
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: 'postgres',
+  dialectModule: pg
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida correctamente.');
+  })
+  .catch(err => {
+    console.error('Error al conectar a la base de datos:', err);
+  });
+
+module.exports = sequelize;
